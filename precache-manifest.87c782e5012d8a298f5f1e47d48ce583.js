b2ddc10cdb89e8370bcdbdbbe15f3168e2ad2079
Ashutosh Kendurkar <ashutosh.kendurkar@gmail.com>
2019-08-04 19:51:13 +0530

    Updates

---
+++ precache-manifest.87c782e5012d8a298f5f1e47d48ce583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361f028a1a53615e3d180f49f54ac68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f2c1c6d32ec98eb3ed",
+    "url": "/static/css/main.606598e5.chunk.css"
+  },
+  {
+    "revision": "6943421546a75957c807",
+    "url": "/static/js/2.02584be4.chunk.js"
+  },
+  {
+    "revision": "27f2c1c6d32ec98eb3ed",
+    "url": "/static/js/main.0c86be49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
